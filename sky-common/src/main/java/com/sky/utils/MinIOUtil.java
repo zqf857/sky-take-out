d0adc893adca148e7c4a9e601119f13463195e1b
zqf857 <2259991332@qq.com>
2025-09-25 11:20:18 +0800

    MinIO文件上传公共接口实现

--- sky-common/src/main/java/com/sky/utils/MinIOUtil.java
+++ sky-common/src/main/java/com/sky/utils/MinIOUtil.java
@@ -2,12 +2,16 @@ package com.sky.utils;
 
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
-import io.minio.errors.MinioException;
+import io.minio.errors.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @Data
 @AllArgsConstructor
@@ -19,44 +23,56 @@ public class MinIOUtil {
     private String secretKey;
     private String bucketName;
 
-    /**
-     * 文件上传
-     *
-     * @param bytes      文件字节数组
-     * @param objectName Minio中的对象名
-     * @return 文件访问路径
-     */
-    public String upload(byte[] bytes, String objectName) {
-        try {
-            // 创建 MinioClient
-            MinioClient minioClient = MinioClient.builder()
-                    .endpoint(url)
-                    .credentials(accessKey, secretKey)
-                    .build();
+    private MinioClient minioClient;
 
-            // 上传文件
+    public MinIOUtil(String url, String accessKey, String secretKey, String bucketName){
+        this.url = url;
+        this.accessKey = accessKey;
+        this.secretKey = secretKey;
+        this.bucketName = bucketName;
+
+        this.minioClient = MinioClient.builder()
+                .endpoint(url)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+
+    /**
+     * 上传文件
+     * @param file
+     * @param objectName
+     * @return
+     */
+    public String upload(MultipartFile file, String objectName) {
+        try {
             minioClient.putObject(
                     PutObjectArgs.builder()
                             .bucket(bucketName)
                             .object(objectName)
-                            .stream(new ByteArrayInputStream(bytes), bytes.length, -1)
-                            .contentType("application/octet-stream") // 可根据文件类型修改
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .contentType(file.getContentType())
                             .build()
             );
-
-            // Minio 访问路径规则 http://url/bucketName/objectName
-            String fileUrl = String.format("%s/%s/%s", url, bucketName, objectName);
-
-            log.info("文件上传到: {}", fileUrl);
-
-            return fileUrl;
-
-        } catch (MinioException e) {
-            log.error("上传到 MinIO 出错: {}", e.getMessage(), e);
-            throw new RuntimeException("Minio 文件上传失败", e);
-        } catch (Exception e) {
-            log.error("上传到 MinIO 出现异常: {}", e.getMessage(), e);
-            throw new RuntimeException("Minio 文件上传失败", e);
+            log.info("文件上传成功: {}/{}", bucketName, objectName);
+            return url + "/" + bucketName + "/" + objectName;
+        } catch (ErrorResponseException e) {
+            throw new RuntimeException(e);
+        } catch (InsufficientDataException e) {
+            throw new RuntimeException(e);
+        } catch (InternalException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidResponseException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (ServerException e) {
+            throw new RuntimeException(e);
+        } catch (XmlParserException e) {
+            throw new RuntimeException(e);
         }
     }
 }
---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,49 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.MinIOUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * 通用接口
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private MinIOUtil minIOUtil;
+    /**
+     * 文件上传接口
+     * @return
+     */
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(@RequestBody MultipartFile file){
+        log.info("文件上传: {}", file);
+
+        // 原始文件名
+        String originalFilename = file.getOriginalFilename();
+        // 截取原始文件名的后缀拓展名
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String objectName = UUID.randomUUID().toString() + extension;
+
+        // 文件的请求路径
+        String filePath = minIOUtil.upload(file, objectName);
+        return Result.success(filePath);
+
+    }
+}
