dccd56b57cce0e3ba513781d83ed57d2a2d1c5d2
pancake <jiangbingzhu666@gmail.com>
2025-10-11 17:47:10 +0800

    菜品分页查询和删除菜品业务代码开发

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
 import com.sky.utils.MinIOUtil;
 import io.swagger.annotations.Api;
@@ -35,15 +36,20 @@ public class CommonController {
     public Result<String> upload(@RequestBody MultipartFile file){
         log.info("文件上传: {}", file);
 
-        // 原始文件名
-        String originalFilename = file.getOriginalFilename();
-        // 截取原始文件名的后缀拓展名
-        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
-        String objectName = UUID.randomUUID().toString() + extension;
+        try {
+            // 原始文件名
+            String originalFilename = file.getOriginalFilename();
+            // 截取原始文件名的后缀拓展名
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            String objectName = UUID.randomUUID().toString() + extension;
 
-        // 文件的请求路径
-        String filePath = minIOUtil.upload(file, objectName);
-        return Result.success(filePath);
+            // 文件的请求路径
+            String filePath = minIOUtil.upload(file, objectName);
+            return Result.success(filePath);
 
+        } catch (RuntimeException e) {
+            log.error("文件上传失败: {}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
 }
---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,29 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
+
+public interface DishService {
+
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    void saveWithFlavor(DishDTO dishDTO);
+
+    /**
+     * 菜品分页查询
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 批量删除菜品
+     * @param ids
+     */
+    void deleteByIds(List<Long> ids);
+}
